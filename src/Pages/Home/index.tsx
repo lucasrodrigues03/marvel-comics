6ba93b9bee44920b91c8fccbef528cba920cc99b
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-10-07 00:05:49 -0300

    add pagination

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -5,17 +5,58 @@ import { HomeContainer } from './styles'
 interface Comic {
   id: string
   title: string
-  imageUrl: string
+  thumbnail: string
 }
 
 export function Home() {
   const [comics, setComics] = useState<Comic[]>([])
 
-  useEffect(() => {
-    api.get(`/comics`).then((response) => {
-      console.log(response.data)
-      setComics(response.data.data.results)
+  const [paginationData, setPaginationData] = useState({
+    offset: 1,
+    total: 0,
+    count: 0,
+    limit: 0,
+  })
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const totalPages = Math.floor(paginationData.total / paginationData.limit)
+
+  function handleGetComics(page: number = 1) {
+    api.get(`/comics`, { params: { offset: page } }).then((response) => {
+      const results = response.data.data.results.map(
+        (result: Record<string, any>) => {
+          return {
+            ...result,
+            thumbnail: `${result.thumbnail.path}.${result.thumbnail.extension}`,
+          }
+        },
+      )
+      setComics(results)
+      setPaginationData({
+        offset: response.data.data.offset,
+        total: response.data.data.total,
+        count: response.data.data.count,
+        limit: response.data.data.limit,
+      })
     })
+  }
+
+  function handleNextPage() {
+    if (currentPage === totalPages) return
+    handleGetComics(currentPage + 1)
+
+    setCurrentPage((prevState) => prevState + 1)
+  }
+
+  function previousPage() {
+    if (currentPage <= 1) return
+    handleGetComics(currentPage - 1)
+
+    setCurrentPage((prevState) => prevState - 1)
+  }
+
+  useEffect(() => {
+    handleGetComics()
   }, [])
 
   return (
@@ -24,11 +65,25 @@ export function Home() {
         return (
           <>
             <ul>
-              <li key={comic.id}>{comic.title}</li>
+              <li key={comic.id}>
+                {comic.title}{' '}
+                <img
+                  className="img-comics"
+                  src={comic.thumbnail}
+                  alt="image-comic"
+                />
+              </li>
             </ul>
           </>
         )
       })}
+      <button onClick={previousPage} type="button">
+        Voltar
+      </button>
+      {currentPage} de {totalPages}
+      <button onClick={handleNextPage} type="button">
+        Avançar
+      </button>
     </HomeContainer>
   )
 }
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,3 +1,12 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.div``
+export const HomeContainer = styled.div`
+  background: #f2f2f2;
+
+  .img-comics {
+    width: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
