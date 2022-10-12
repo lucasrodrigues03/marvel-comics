917c0abdb8e27cb4eee149daa1ef748d8fa64819
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-10-12 16:47:29 -0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Header } from './components/Header'
+import { Cart } from './Pages/Cart'
 import { Home } from './Pages/Home'
+import { store } from './store'
 import { GlobalStyle } from './styles/GlobalStyles'
-import { Comic } from './types/comic'
 
-export function App({ description, id, thumbnail, title }: Comic) {
+export function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <GlobalStyle />
+      </BrowserRouter>
+    </Provider>
   )
 }
