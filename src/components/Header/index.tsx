ebf182f2b0679384be54609c400d7674d5c89f09
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-10-12 17:00:49 -0300

    route navigation then loading spinner in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,28 @@
+import { ShoppingCart } from 'phosphor-react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { RootState } from '../../store'
+import { CartState } from '../../store/reducers/cart'
+import { LoadingState } from '../../store/reducers/loading'
+import { Loading } from '../Loading'
 import { HeaderContainer } from './styles'
 
 export function Header() {
+  const navigate = useNavigate()
+  const cart = useSelector<RootState, CartState>((state) => state.cart)
+  const loading = useSelector<RootState, LoadingState>((state) => state.loading)
   return (
     <HeaderContainer>
-      <h1>MARVEL COMICS</h1>
+      <div style={{ width: '50px' }}>{loading.isLoading && <Loading />}</div>
+      <img
+        data-test="header-home"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/128px-Marvel_Logo.svg.png"
+        alt="marvel-logo"
+      />
+      <button onClick={() => navigate('/cart')} type="button">
+        <ShoppingCart size={30} />
+        {cart.products.length !== 0 && cart.products.length}
+      </button>
     </HeaderContainer>
   )
 }
