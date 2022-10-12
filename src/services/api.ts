ca6599c1055710df9bf6a43d6a0a1d4b6b8300e4
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-10-12 16:55:52 -0300

    environment variables in api keys

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,10 @@
 import axios from 'axios'
 
-export const timeStamp = import.meta.env.VITE_APP_TIMESTAMP
-export const apikey = import.meta.env.VITE_APP_API_KEY
-export const hash = import.meta.env.VITE_APP_HASH
-
 export const api = axios.create({
   baseURL: 'http://gateway.marvel.com/v1/public/',
   params: {
-    ts: '1665000255',
-    apikey: 'a9e55eb2ade905057bfef0a711b8ff0b',
-    hash: 'b567cf163765d10aee1cc37b1e90ee2b',
-    offset: 10,
+    ts: import.meta.env.VITE_APP_TIMESTAMP,
+    apikey: import.meta.env.VITE_APP_API_KEY,
+    hash: import.meta.env.VITE_APP_HASH,
   },
 })
