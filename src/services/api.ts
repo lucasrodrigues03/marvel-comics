18d5f3a5fcc25601a98d353c1cbf5bd941cb0a99
lucasrodriges <lucasrodrigues7691@gmail.com>
2022-10-12 23:36:17 -0300

    change baseurl marvel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://gateway.marvel.com/v1/public/',
+  baseURL: 'https://gateway.marvel.com/v1/public/',
   params: {
     ts: import.meta.env.VITE_APP_TIMESTAMP,
     apikey: import.meta.env.VITE_APP_API_KEY,
